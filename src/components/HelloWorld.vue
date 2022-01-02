962f9c9b71dce118b691782da155116c61d2178e
roycehoe <royceinbox@hotmail.com>
2022-01-02 12:23:25 +0000

    TEMPLATE SAVEPOINT; implement working docker container which can communicate with frontend docker container

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import router from "@/router";
+import { ErrorCode, ErrorMsg } from "@/services/errors";
+import { AuthUserRequest, AuthUserResponse, getAuthUserResponse } from "@/services/user/getAuthUserResponse";
+import { client } from "@/services";
+
+async function helloWorld() {
+  return await client.post("https://jsonplaceholder.typicode.com/todos/1")
+}
+
+</script>
+
+<template>
+  <p class="hello-world">{{ helloWorld() }}</p>
+</template>
+
+<style scoped lang="scss">
+.hello-world {
+  background-color: black;
+  color: white;
+}
+</style>
