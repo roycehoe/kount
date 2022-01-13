359a76a612a4e59093321cacd6a779ee373f429b
roycehoe <royceinbox@hotmail.com>
2022-01-13 21:35:45 +0000

    Integrate create survey with frontend

--- src/components/SurveyCreateForm.vue
+++ src/components/SurveyCreateForm.vue
@@ -1,27 +1,18 @@
-<script lang="ts" setup>import { ref } from 'vue';
-
-
-enum questionTypes {
-    Text = "TEXT",
-    MultipleChoice = "MULTIPLE_CHOICE"
-}
-
-
-interface SurveyQuestion {
-    type: questionTypes
-    title: string
-    optionCount?: number
-    options?: Array<string>
-}
-
-interface SurveyQuestionRequest {
-    questions: Array<SurveyQuestion>
-}
-
+<script lang="ts" setup>import { CreateSurveyQuestion, CreateSurveyRequest, getCreateSurveyResponse } from '@/services/survey/getCreateSurveyResponse';
+import { ref } from 'vue';
 
 const isAddQuestion = ref(true)
-const surveyQuestionForm = ref({} as SurveyQuestionRequest)
-const createQuestionForm = ref({} as SurveyQuestion)
+const createSurveyForm = ref({ title: "", questions: [] } as CreateSurveyRequest)
+const createQuestionForm = ref({
+} as CreateSurveyQuestion)
+
+
+async function createSurvey(): Promise<void> {
+    const { ok: isSuccessful, val: response } = await getCreateSurveyResponse(createSurveyForm.value)
+    //To add in error handling here
+}
+
+
 
 </script>
 
@@ -30,42 +21,30 @@ const createQuestionForm = ref({} as SurveyQuestion)
         <label class="label">
             <span class="label-text font-bold">New survey name</span>
         </label>
-        <input type="text" placeholder="Name" class="input input-bordered" />
+        <input
+            type="text"
+            placeholder="Name"
+            class="input input-bordered"
+            v-model="createSurveyForm.title"
+        />
     </div>
 
     <div class="divider"></div>
-    <p>create question form {{ createQuestionForm }}</p>
 
     <div v-if="isAddQuestion" class="flex flex-col">
-        <select v-model="createQuestionForm.type" class="select select-bordered w-full max-w-xs">
-            <option disabled selected>Question type</option>
-            <option :value="questionTypes.Text">Text</option>
-            <option :value="questionTypes.MultipleChoice">Multiple Choice</option>
-        </select>
-
-        <span class="label-text font-bold">Question</span>
+        <span class="label-text font-bold m-2">Question</span>
         <input
             type="text"
             placeholder="Title"
             class="input input-bordered"
             v-model="createQuestionForm.title"
         />
-
-        <div v-if="createQuestionForm.type === questionTypes.MultipleChoice" class="mt-5">
-            <div>
-                <input
-                    placeholder="Option"
-                    class="input input-bordered w-full"
-                    v-model="createQuestionForm.options"
-                />
-            </div>
-
-            <button class="btn btn-outline btn-sm">Add Option</button>
-            <button class="btn btn-outline btn-sm hover:bg-red-500">Remove Option</button>
-        </div>
     </div>
 
-    <button class="hover:bg-neutral-100 flex flex-row my-5" @click="surveyQuestionForm.count += 1">
+    <button
+        class="hover:bg-neutral-100 flex flex-row my-5"
+        @click="createSurveyForm.questions.push({ title: createQuestionForm.title })"
+    >
         <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-6 w-6"
@@ -82,7 +61,11 @@ const createQuestionForm = ref({} as SurveyQuestion)
         </svg>
         <p>Add question</p>
     </button>
-    <button class="hover:bg-neutral-100 flex flex-row my-5" @click="surveyQuestionForm.count -= 1">
+
+    <button
+        class="hover:bg-neutral-100 flex flex-row my-5"
+        @click="createSurveyForm.questions.pop()"
+    >
         <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-6 w-6"
@@ -90,23 +73,23 @@ const createQuestionForm = ref({} as SurveyQuestion)
             viewBox="0 0 24 24"
             stroke="currentColor"
         >
-            <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="{2}"
-                d="M12 4v16m8-8H4"
-            />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="{2}" d="M20 12H4" />
         </svg>
         <p>Remove question</p>
     </button>
     <div class="divider"></div>
+    <p>Question list</p>
+    <li v-for="question in createSurveyForm.questions" :key="question.title">{{ question.title }}</li>
+
+    <div class="divider"></div>
 
     <div>
         <button
+            @click="createSurvey"
             class="bg-green-500 mw-2 w-full h-10 rounded-md hover:bg-green-600 my-3"
         >Create Survey</button>
     </div>
-    <p>{{ surveyQuestionForm }}</p>
+    <p>survey form {{ createSurveyForm }}</p>
 </template>
 
 <style scoped lang="scss">
