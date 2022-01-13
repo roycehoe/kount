d4b75734bcade92cae22b4362d29608f336eb732
roycehoe <royceinbox@hotmail.com>
2022-01-13 12:16:45 +0000

    Save point; Implementing separation of survey create menu and survey create form

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import CreateAccount from '@/components/CreateAccount.vue'
 import Login from '@/components/Login.vue'
-import Dashboard from '@/components/Dashboard.vue'
+import Dashboard from '@/views/Dashboard.vue'
 
 
 const routes = [
