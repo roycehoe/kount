aa998b8b197c4dc09766e24497b1d6c43fe426fa
roycehoe <royceinbox@hotmail.com>
2022-01-13 18:04:02 +0000

    Implement text and multiple choice barebone toggle on dashboard

--- src/components/SurveyCreateForm.vue
+++ src/components/SurveyCreateForm.vue
@@ -1,6 +1,17 @@
 <script lang="ts" setup>import { ref } from 'vue';
 
-const isAddQuestion = ref(false)
+const isAddQuestion = ref(true)
+const questionOptions = ref([] as Array<any>)
+const questionTitle = ref("")
+const questionType = ref("Text")
+
+function addOption() {
+    questionOptions.value.push({ option: '' })
+}
+
+function removeOption() {
+    questionOptions.value.pop()
+}
 
 </script>
 
@@ -12,11 +23,41 @@ const isAddQuestion = ref(false)
         <input type="text" placeholder="Name" class="input input-bordered" />
     </div>
 
-    <div v-if="isAddQuestion">
-        <span class="label-text font-bold">Question type</span>
+    <div class="divider"></div>
+
+    <div v-if="isAddQuestion" class="flex flex-col">
+        <div class="tabs">
+            <button class="tab tab-bordered" @click="questionType = 'Text'">Text</button>
+            <button
+                class="tab tab-bordered"
+                @click="questionType = 'Multiple choice'"
+            >Multiple choice</button>
+        </div>
+
+        <p>question title{{ questionTitle }}</p>
+        <p>question options{{ questionOptions }}</p>
+
+        <span class="label-text font-bold">Question</span>
+        <input type="text" placeholder="Title" class="input input-bordered" v-model="questionTitle" />
+
+        <div v-if="questionType === 'Multiple choice'" class="mt-5">
+            <div v-for="(questionOption, index) in questionOptions">
+                <input
+                    v-model="questionOption.option"
+                    placeholder="Option"
+                    class="input input-bordered w-full"
+                />
+            </div>
+
+            <button @click="addOption" class="btn btn-outline btn-sm">Add Option</button>
+            <button
+                @click="removeOption"
+                class="btn btn-outline btn-sm hover:bg-red-500"
+            >Remove Option</button>
+        </div>
     </div>
 
-    <button class="hover:bg-neutral-100 flex flex-row my-5" @click="isAddQuestion = !isAddQuestion">
+    <!-- <button class="hover:bg-neutral-100 flex flex-row my-5" @click="isAddQuestion = !isAddQuestion">
         <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-6 w-6"
@@ -32,7 +73,8 @@ const isAddQuestion = ref(false)
             />
         </svg>
         <p>Add question</p>
-    </button>
+    </button>-->
+    <div class="divider"></div>
 
     <div>
         <button
