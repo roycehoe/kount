09e92c1f75da8718f45ae0ee02308b41eba914b9
roycehoe <royceinbox@hotmail.com>
2022-01-13 20:19:41 +0000

    Abandon multiple choice to create only text input

--- src/components/SurveyCreateForm.vue
+++ src/components/SurveyCreateForm.vue
@@ -1,18 +1,27 @@
 <script lang="ts" setup>import { ref } from 'vue';
 
+
+enum questionTypes {
+    Text = "TEXT",
+    MultipleChoice = "MULTIPLE_CHOICE"
+}
+
+
+interface SurveyQuestion {
+    type: questionTypes
+    title: string
+    optionCount?: number
+    options?: Array<string>
+}
+
+interface SurveyQuestionRequest {
+    questions: Array<SurveyQuestion>
+}
+
+
 const isAddQuestion = ref(true)
-const questionOptions = ref([] as Array<any>)
-const questionTitle = ref("")
-const questionType = ref("Text")
-const questionCount = ref(0)
-
-function addOption() {
-    questionOptions.value.push({ option: '' })
-}
-
-function removeOption() {
-    questionOptions.value.pop()
-}
+const surveyQuestionForm = ref({} as SurveyQuestionRequest)
+const createQuestionForm = ref({} as SurveyQuestion)
 
 </script>
 
@@ -25,40 +34,38 @@ function removeOption() {
     </div>
 
     <div class="divider"></div>
+    <p>create question form {{ createQuestionForm }}</p>
 
-    <div v-if="isAddQuestion" class="flex flex-col" v-for="n in questionCount">
-        <div class="tabs">
-            <button class="tab tab-bordered" @click="questionType = 'Text'">Text</button>
-            <button
-                class="tab tab-bordered"
-                @click="questionType = 'Multiple choice'"
-            >Multiple choice</button>
-        </div>
-
-        <p>question title{{ questionTitle }}</p>
-        <p>question options{{ questionOptions }}</p>
+    <div v-if="isAddQuestion" class="flex flex-col">
+        <select v-model="createQuestionForm.type" class="select select-bordered w-full max-w-xs">
+            <option disabled selected>Question type</option>
+            <option :value="questionTypes.Text">Text</option>
+            <option :value="questionTypes.MultipleChoice">Multiple Choice</option>
+        </select>
 
         <span class="label-text font-bold">Question</span>
-        <input type="text" placeholder="Title" class="input input-bordered" v-model="questionTitle" />
+        <input
+            type="text"
+            placeholder="Title"
+            class="input input-bordered"
+            v-model="createQuestionForm.title"
+        />
 
-        <div v-if="questionType === 'Multiple choice'" class="mt-5">
-            <div v-for="(questionOption, index) in questionOptions">
+        <div v-if="createQuestionForm.type === questionTypes.MultipleChoice" class="mt-5">
+            <div>
                 <input
-                    v-model="questionOption.option"
                     placeholder="Option"
                     class="input input-bordered w-full"
+                    v-model="createQuestionForm.options"
                 />
             </div>
 
-            <button @click="addOption" class="btn btn-outline btn-sm">Add Option</button>
-            <button
-                @click="removeOption"
-                class="btn btn-outline btn-sm hover:bg-red-500"
-            >Remove Option</button>
+            <button class="btn btn-outline btn-sm">Add Option</button>
+            <button class="btn btn-outline btn-sm hover:bg-red-500">Remove Option</button>
         </div>
     </div>
 
-    <button class="hover:bg-neutral-100 flex flex-row my-5" @click="questionCount += 1">
+    <button class="hover:bg-neutral-100 flex flex-row my-5" @click="surveyQuestionForm.count += 1">
         <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-6 w-6"
@@ -75,7 +82,7 @@ function removeOption() {
         </svg>
         <p>Add question</p>
     </button>
-    <button class="hover:bg-neutral-100 flex flex-row my-5" @click="questionCount -= 1">
+    <button class="hover:bg-neutral-100 flex flex-row my-5" @click="surveyQuestionForm.count -= 1">
         <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-6 w-6"
@@ -99,6 +106,7 @@ function removeOption() {
             class="bg-green-500 mw-2 w-full h-10 rounded-md hover:bg-green-600 my-3"
         >Create Survey</button>
     </div>
+    <p>{{ surveyQuestionForm }}</p>
 </template>
 
 <style scoped lang="scss">
