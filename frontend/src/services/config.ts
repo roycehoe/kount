91814183f458322485e52b2179155c5f4760d3da
roycehoe <royceinbox@hotmail.com>
2022-01-20 05:21:55 +0000

    Implement working deployment

--- frontend/src/services/config.ts
+++ frontend/src/services/config.ts
@@ -11,4 +11,4 @@ function getBaseURL(envValue: string | undefined, defaultValue: string): string 
     return defaultValue
 }
 
-export const baseURL = getBaseURL(envFile.VITE_BASE_URL, "http://0.0.0.0:8080/")
+export const baseURL = getBaseURL(envFile.VITE_BASE_URL, "http://localhost:8080/")
